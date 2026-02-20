b89988d956141cea1fffa5b9aaf29429587f501a
Martin-Herrero98 <ferrermarti9@gmail.com>
2026-02-20 20:19:53 +0000

    Last

--- src/app.js
+++ src/app.js
@@ -5,7 +5,22 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let pronombres = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let dom = ['.es', '.com', '.org'];
+
+for (let a of pronombres){
+  for (let b of adj){
+    for (let c of noun){
+      for (let d of dom){
+        console.log(`${a}${b}${c}${d}`)
+      }
+    }
+  }
+}
